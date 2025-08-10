4e702d82bf6bd8799b1cd12d56ff8894b99d4ef6
Garret Nelson <garretnelson368@gmail.com>
2025-08-10 14:50:05 +0700

    fix: Update dependencies, add secret scanning, and fix project conflicts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  '*.{js,ts,jsx,tsx}': ['eslint --fix', 'prettier --write'],
-  '*.{json,md,yml,yaml}': ['prettier --write'],
+  '*.{js,ts,jsx,tsx}': ['eslint --fix', 'prettier --write', 'secretlint'],
+  '*.{json,md,yml,yaml}': ['prettier --write', 'secretlint'],
+  '*': ['secretlint'],
 };

1148a8656af2c49584617823f4e9940b07d0239d
redrover13 <garretnelson368@gmail.com>
2025-09-04 06:25:42 +0700

    Apply reviewer fixes: remove duplicate aliases, update Jest config, align Cloud Functions module settings

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from 'jest';
 import { getJestProjectsAsync } from '@nx/jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.base.json';
 
 export default async (): Promise<Config> => ({
   projects: await getJestProjectsAsync(),
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
 });
